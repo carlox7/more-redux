47bfa8dcffbb9705630715253b180bba49370866
Carlo Santos <carlox7@gmail.com>
2017-08-09 15:29:15 -0700

    finished kanban functionality, starting tests

--- kanban/src/component/card-item/index.js
+++ kanban/src/component/card-item/index.js
@@ -3,22 +3,50 @@ import {connect} from 'react-redux'
 
 import Draggable from '../draggable'
 import CardForm from '../card-form'
+import {renderIf} from '../../lib/util.js'
 import {cardUpdate, cardDelete} from '../../action/card-actions.js'
 
 class CardItem extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      editing: false,
+    }
+
+    this.handleCardUpdate = this.handleCardUpdate.bind(this)
+  }
+
+  handleCardUpdate(card){
+    this.props.cardUpdate(card)
+    this.setState({editing:false})
+  }
+
   render(){
     let {card, cardDelete, cardUpdate} = this.props
     return(
       <li className='card-item'>
-        <Draggable dataTransferItem={card}>
 
-          <p>{card.content}</p>
-          <button onClick={() => cardDelete(card)}> delete </button>
-          <CardForm
-            card={card}
-            buttonText='update card'
-            onComplete={cardUpdate}
-          />
+        <Draggable dataTransferItem={card}>
+          {renderIf(!this.state.editing,
+            <div onDoubleClick={() => this.setState({editing: true})}>
+              <p>{card.content}</p>
+              <button onClick={() => cardDelete(card)}> delete </button>
+            </div>
+          )}
+
+          {renderIf(this.state.editing,
+            <div>
+              <CardForm
+                card={card}
+                buttonText='update card'
+                onComplete={this.handleCardUpdate}
+              />
+              <button
+                onClick={() => this.setState({editing: false})}>
+                cancel
+              </button>
+            </div>
+          )}
         </Draggable>
       </li>
     )
--- kanban/src/component/dropzone/index.js
+++ kanban/src/component/dropzone/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {classToggler} from '../../lib/util.js'
 
 //props
 //onComplete -- will invoke on Drop with the dataTransferItem
@@ -6,8 +7,22 @@ import React from 'react'
 class Dropzone extends React.Component {
   constructor(props){
     super(props)
+    this.state = {
+      dropReady: false,
+    }
+
     this.handleDragOver = this.handleDragOver.bind(this)
     this.handleDrop = this.handleDrop.bind(this)
+    this.handleDragEnter = this.handleDragEnter.bind(this)
+    this.handleDragLeave = this.handleDragLeave.bind(this)
+  }
+
+  handleDragEnter(e){
+    this.setState({dropReady: true})
+  }
+
+  handleDragLeave(e){
+    this.setState({dropReady: false})
   }
 
   handleDragOver(e){
@@ -22,15 +37,25 @@ class Dropzone extends React.Component {
     } catch (error){
       this.props.onComplete(error)
     }
+    this.setState({dropReady: false})
   }
 
   render(){
+    let className = classToggler({
+      'dropzone': true,
+      'drop-ready': this.state.dropReady,
+    })
     return(
       <div
-        className='dropzone'
+        className={className}
         onDragOver={this.handleDragOver}
-        onDrop={this.handleDrop}>
+        onDrop={this.handleDrop}
+        onDragEnter={this.handleDragEnter}
+        onDragLeave={this.handleDragLeave}
+      >
+
         {this.props.children}
+
       </div>
 
     )
